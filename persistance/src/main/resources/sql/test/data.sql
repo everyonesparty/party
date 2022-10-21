2218785ab1adf58334475d2b0afceacb5a6b0161
rnjstjdgh <gshgsh0831@daum.net>
2022-10-21 23:04:58 +0900

    test 하위의 초기화쿼리 제거

--- persistance/src/main/resources/sql/test/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO current_ott_status (ott_name, is_used, ott_status) VALUES
-                                                      ('NETFLIX', true, 'IMMEDIATE_MATCHING'),
-                                                      ('WAVVE', true, 'IMMEDIATE_MATCHING'),
-                                                      ('WATCHA', true, 'IMMEDIATE_MATCHING'),
-                                                      ('LAFTEL', true, 'IMMEDIATE_MATCHING'),
-                                                      ('TVING', true, 'NORMAL'),
-                                                      ('DISNEY', true, 'CONGESTION');
